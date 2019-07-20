9d2a3f762f3a48e55f9dc7b5b690154fe7f7dadc
Kamil Lelonek <squixy.sln@gmail.com>
2019-07-20 19:23:06 +0200

    Configure readiness probe

--- config/config.exs
+++ config/config.exs
@@ -2,4 +2,6 @@ use Mix.Config
 
 config :healthchex,
   liveness_path: "/health/live",
-  liveness_response: "OK"
+  liveness_response: "OK",
+  readiness_path: "/health/ready",
+  readiness_response: "OK"
